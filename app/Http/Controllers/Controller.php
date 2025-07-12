e62b18bfc16e94194036f04359701d4362db9637
Ari12sadewa <43287768+Ari12sadewa@users.noreply.github.com>
2025-07-12 20:33:44 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     // Di controller (sementara untuk debug)
-\Log::info('Request headers:', request()->headers->all());
-\Log::info('Response headers:', response()->headers->all());
+// \Log::info('Request headers:', request()->headers->all());
+// \Log::info('Response headers:', response()->headers->all());
 
 }

98c9b97118889bab8ff2aaaac768478193422e1a
Ari12sadewa <43287768+Ari12sadewa@users.noreply.github.com>
2025-07-12 19:11:12 +0700

    Update cors.php

--- cors.php
+++ cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173','https://backendlaravel-production-8c8a.up.railway.app'],
+    'allowed_origins' => ['http://localhost:5173','https://frontend-khaki-chi-95.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
